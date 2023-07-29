4126c87ae2c31c1a4e4b1fa9b1ed65f2c7a237f4
mauriciojfranco <mauriciojfranco@live.com>
2023-07-28 21:58:39 -0300

    Instalacion de bootstrap y componente home

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
+  /*{
     path: 'dashboard',
     loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule),
+  },*/
+  {
+    path: 'home',
+    redirectTo: 'home'
   },
+
   {
     path: '**',
-    redirectTo: 'dashboard'
+    redirectTo: 'home'
   }
 ];
 
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,13 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './modules/navbar/navbar.component';
+import { LoginComponent } from './modules/login/login.component';
+import { HomeComponent } from './modules/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,   
     AppRoutingModule
   ],
   providers: [],
